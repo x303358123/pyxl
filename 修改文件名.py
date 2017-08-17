569ecd2f2ffa1ead28d2d17d586cf8873e18aa44
x303358123 <378853159@qq.com>
2017-08-17 17:22:07 +0800

    1

---
+++ 修改文件名.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+import os
+import os.path
+file = []
+for root,dirs,files in os.walk('E:\data\HGHG-NEWOBJ'):
+	if files!=[]:
+		for file1 in files:
+			name,end = os.path.splitext(file1)
+			file.append(name)
+for name1 in file:
+	aa = name1.split('_')
+	for root, dirs, files in os.walk('E:\data\HGHG-NEWOBJ'):
+		for dirs1 in dirs:
+			if dirs1 == aa[1]:
+				root1 = os.path.join(root,dirs1)
+				root2 = os.path.join(root,aa)
+				os.rename(root1, root2)
